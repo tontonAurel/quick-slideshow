b882695ee47b7bcb2c99f66dfc78b70485d92182
Aurelien RUARO <Aurel@aurel-mba.ws.lab.easyflirt.com>
2015-12-15 09:13:08 +0100

    Nouvel lib

--- public/index.php
+++ public/index.php
@@ -1,114 +1,339 @@
 <?php
+session_start();
+if (isset($_POST['login'])) {
+  $_SESSION['logged'] = $_POST['login'] == 'login' && $_POST['password'] == 'password';
+  header('Location: index.php');
+  exit;
+}
 $images = glob("./images/*.jpg");
 ?>
 <!doctype html>
 <html lang="fr">
 <head>
   <meta charset="utf-8">
-  <title>Titre de la page</title>
-  <link rel="stylesheet" href="css/flexslider.css">
+  <title>Slideshow - Demo</title>
+  <link rel="stylesheet" href="./css/photoswipe.css">
+  <link rel="stylesheet" href="./css/default-skin/default-skin.css">
   <style type="text/css">
-  body {
-    padding:10px;
-    margin:0;
-    overflow-x: hidden;
-  }
-  .flexslider {
-    padding:0;
-    margin:0;
-    border:0;
-  }
-  .flexslider .flex-direction-nav .flex-prev, .flexslider .flex-direction-nav .flex-next {
-    display: none;
-   }
-.flexslider:hover .flex-direction-nav .flex-prev {
- display: block;
+  .my-gallery {
+  width: 100%;
+  float: left;
 }
-.flexslider:hover .flex-direction-nav .flex-next {
+.my-gallery img {
+  width: 100%;
+  height: auto;
+}
+.my-gallery figure {
   display: block;
+  float: left;
+  margin: 0 5px 5px 0;
+  width: 150px;
 }
-#slider{
-    margin: auto;
+.my-gallery figcaption {
+  display: none;
 }
-#slider .slides > li {
-    background-attachment: local;
-    background-size: cover;
-    height: 500px;
-}
-
-#slider {
-    margin-bottom: 20px;
-}
-
-  </style>
+</style>
 </head>
 <body>
-<!-- Place somewhere in the <body> of your page -->
-<div id="slider" class="flexslider">
-          <ul class="slides">
-            <?php foreach($images as $image) : ?>
-            <li style="background-image:url(<?php echo $image ?>)">
-            </li>
-            <?php endforeach; ?>
-          </ul>
-        </div>
-        <div id="carousel" class="flexslider">
-          <ul class="slides">
-          <?php foreach($images as $image) : ?>
-            <li>
-                <img src="<?php echo $image ?>" />
-                </li>
-            <?php endforeach; ?>
-            
-          </ul>
-        </div>
+  <?php if (!isset($_SESSION['logged'])) : ?>
+    <form method="post" action="index.php">
+      <p><label>Login</label><input type="text" name="login"></p>
+      <p><label>Mot de passe</label><input type="password" name="password"></p>
+      <p><input type="submit" value="Valider"></p>
+    </form>
+<?php else : ?>
+  <div class="my-gallery" itemscope itemtype="http://schema.org/ImageGallery">
+    <?php
+      if (file_exists('../cache/cache.html')) :
+        $cache = file_get_contents('../cache/cache.html');
+        echo "<!-- from cache -->";
+        echo $cache;
+      else :
+        echo "<!-- no cache -->";
+        ob_start();
+        foreach ($images as $image) :
+        $i = getimagesize($image);
+        echo '<figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+          <a href="'.$image.'" itemprop="contentUrl" data-size="' . $i[0]. 'x'. $i[1] .'">
+              <img src="'.$image.'" itemprop="thumbnail" alt="Image description" />
+          </a>
+        </figure>';
+        endforeach;
+        $data = ob_get_contents();
+        file_put_contents('../cache/cache.html', $data);
+        ob_end_clean();
+        echo $data;
+      endif;  ?>
 
- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.min.js">\x3C/script>')</script>
+    </div>
+  <!-- Root element of PhotoSwipe. Must have class pswp. -->
+  <div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
 
-  <!-- FlexSlider -->
-  <script defer src="js/jquery.flexslider.js"></script>
+      <!-- Background of PhotoSwipe.
+           It's a separate element, as animating opacity is faster than rgba(). -->
+      <div class="pswp__bg"></div>
 
-  <script type="text/javascript">
-    $(function(){
-      SyntaxHighlighter.all();
-    });
-    $(window).load(function(){
-      $('#carousel').flexslider({
-        animation: "slide",
-        controlNav: false,
-        animationLoop: false,
-        slideshow: false,
-        itemWidth: 210,
-        itemMargin: 5,
-        asNavFor: '#slider'
-      });
+      <!-- Slides wrapper with overflow:hidden. -->
+      <div class="pswp__scroll-wrap">
 
-      $('#slider').flexslider({
-        animation: "slide",
-        controlNav: false,
-        animationLoop: false,
-        slideshow: false,
-        sync: "#carousel",
-        start: function(slider){
-          $('body').removeClass('loading');
+          <!-- Container that holds slides. PhotoSwipe keeps only 3 slides in DOM to save memory. -->
+          <!-- don't modify these 3 pswp__item elements, data is added later on. -->
+          <div class="pswp__container">
+              <div class="pswp__item"></div>
+              <div class="pswp__item"></div>
+              <div class="pswp__item"></div>
+          </div>
+
+          <!-- Default (PhotoSwipeUI_Default) interface on top of sliding area. Can be changed. -->
+          <div class="pswp__ui pswp__ui--hidden">
+
+              <div class="pswp__top-bar">
+
+                  <!--  Controls are self-explanatory. Order can be changed. -->
+
+                  <div class="pswp__counter"></div>
+
+                  <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
+
+                  <!-- Preloader demo http://codepen.io/dimsemenov/pen/yyBWoR -->
+                  <!-- element will get class pswp__preloader--active when preloader is running -->
+                  <div class="pswp__preloader">
+                      <div class="pswp__preloader__icn">
+                        <div class="pswp__preloader__cut">
+                          <div class="pswp__preloader__donut"></div>
+                        </div>
+                      </div>
+                  </div>
+              </div>
+
+              <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
+                  <div class="pswp__share-tooltip"></div>
+              </div>
+
+              <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
+              </button>
+
+              <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)">
+              </button>
+
+              <div class="pswp__caption">
+                  <div class="pswp__caption__center"></div>
+              </div>
+
+            </div>
+
+          </div>
+
+  </div>
+        <script src="./js/photoswipe.min.js"></script>
+        <script src="./js/photoswipe-ui-default.min.js"></script>
+        <script type="text/javascript">
+        var initPhotoSwipeFromDOM = function(gallerySelector) {
+
+    // parse slide data (url, title, size ...) from DOM elements
+    // (children of gallerySelector)
+    var parseThumbnailElements = function(el) {
+        var thumbElements = el.childNodes,
+            numNodes = thumbElements.length,
+            items = [],
+            figureEl,
+            linkEl,
+            size,
+            item;
+
+        for(var i = 0; i < numNodes; i++) {
+
+            figureEl = thumbElements[i]; // <figure> element
+
+            // include only element nodes
+            if(figureEl.nodeType !== 1) {
+                continue;
+            }
+
+            linkEl = figureEl.children[0]; // <a> element
+
+            size = linkEl.getAttribute('data-size').split('x');
+
+            // create slide object
+            item = {
+                src: linkEl.getAttribute('href'),
+                w: parseInt(size[0], 10),
+                h: parseInt(size[1], 10)
+            };
+
+
+
+            if(figureEl.children.length > 1) {
+                // <figcaption> content
+                item.title = figureEl.children[1].innerHTML;
+            }
+
+            if(linkEl.children.length > 0) {
+                // <img> thumbnail element, retrieving thumbnail url
+                item.msrc = linkEl.children[0].getAttribute('src');
+            }
+
+            item.el = figureEl; // save link to element for getThumbBoundsFn
+            items.push(item);
         }
-      });
-    });
-  </script>
+
+        return items;
+    };
+
+    // find nearest parent element
+    var closest = function closest(el, fn) {
+        return el && ( fn(el) ? el : closest(el.parentNode, fn) );
+    };
+
+    // triggers when user clicks on thumbnail
+    var onThumbnailsClick = function(e) {
+        e = e || window.event;
+        e.preventDefault ? e.preventDefault() : e.returnValue = false;
+
+        var eTarget = e.target || e.srcElement;
+
+        // find root element of slide
+        var clickedListItem = closest(eTarget, function(el) {
+            return (el.tagName && el.tagName.toUpperCase() === 'FIGURE');
+        });
+
+        if(!clickedListItem) {
+            return;
+        }
+
+        // find index of clicked item by looping through all child nodes
+        // alternatively, you may define index via data- attribute
+        var clickedGallery = clickedListItem.parentNode,
+            childNodes = clickedListItem.parentNode.childNodes,
+            numChildNodes = childNodes.length,
+            nodeIndex = 0,
+            index;
+
+        for (var i = 0; i < numChildNodes; i++) {
+            if(childNodes[i].nodeType !== 1) {
+                continue;
+            }
+
+            if(childNodes[i] === clickedListItem) {
+                index = nodeIndex;
+                break;
+            }
+            nodeIndex++;
+        }
 
 
-  <!-- Syntax Highlighter -->
-  <script type="text/javascript" src="js/shCore.js"></script>
-  <script type="text/javascript" src="js/shBrushXml.js"></script>
-  <script type="text/javascript" src="js/shBrushJScript.js"></script>
 
-  <!-- Optional FlexSlider Additions -->
-  <script src="js/jquery.easing.js"></script>
-  <script src="js/jquery.mousewheel.js"></script>
-  <script defer src="js/demo.js"></script>
+        if(index >= 0) {
+            // open PhotoSwipe if valid index found
+            openPhotoSwipe( index, clickedGallery );
+        }
+        return false;
+    };
 
+    // parse picture index and gallery index from URL (#&pid=1&gid=2)
+    var photoswipeParseHash = function() {
+        var hash = window.location.hash.substring(1),
+        params = {};
 
-</div>
+        if(hash.length < 5) {
+            return params;
+        }
+
+        var vars = hash.split('&');
+        for (var i = 0; i < vars.length; i++) {
+            if(!vars[i]) {
+                continue;
+            }
+            var pair = vars[i].split('=');
+            if(pair.length < 2) {
+                continue;
+            }
+            params[pair[0]] = pair[1];
+        }
+
+        if(params.gid) {
+            params.gid = parseInt(params.gid, 10);
+        }
+
+        return params;
+    };
+
+    var openPhotoSwipe = function(index, galleryElement, disableAnimation, fromURL) {
+        var pswpElement = document.querySelectorAll('.pswp')[0],
+            gallery,
+            options,
+            items;
+
+        items = parseThumbnailElements(galleryElement);
+
+        // define options (if needed)
+        options = {
+
+            // define gallery index (for URL)
+            galleryUID: galleryElement.getAttribute('data-pswp-uid'),
+
+            getThumbBoundsFn: function(index) {
+                // See Options -> getThumbBoundsFn section of documentation for more info
+                var thumbnail = items[index].el.getElementsByTagName('img')[0], // find thumbnail
+                    pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
+                    rect = thumbnail.getBoundingClientRect();
+
+                return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
+            }
+
+        };
+
+        // PhotoSwipe opened from URL
+        if(fromURL) {
+            if(options.galleryPIDs) {
+                // parse real index when custom PIDs are used
+                // http://photoswipe.com/documentation/faq.html#custom-pid-in-url
+                for(var j = 0; j < items.length; j++) {
+                    if(items[j].pid == index) {
+                        options.index = j;
+                        break;
+                    }
+                }
+            } else {
+                // in URL indexes start from 1
+                options.index = parseInt(index, 10) - 1;
+            }
+        } else {
+            options.index = parseInt(index, 10);
+        }
+
+        // exit if index not found
+        if( isNaN(options.index) ) {
+            return;
+        }
+
+        if(disableAnimation) {
+            options.showAnimationDuration = 0;
+        }
+
+        // Pass data to PhotoSwipe and initialize it
+        gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+        gallery.init();
+    };
+
+    // loop through all gallery elements and bind events
+    var galleryElements = document.querySelectorAll( gallerySelector );
+
+    for(var i = 0, l = galleryElements.length; i < l; i++) {
+        galleryElements[i].setAttribute('data-pswp-uid', i+1);
+        galleryElements[i].onclick = onThumbnailsClick;
+    }
+
+    // Parse URL and open gallery if it contains #&pid=3&gid=1
+    var hashData = photoswipeParseHash();
+    if(hashData.pid && hashData.gid) {
+        openPhotoSwipe( hashData.pid ,  galleryElements[ hashData.gid - 1 ], true, true );
+    }
+};
+
+// execute above function
+initPhotoSwipeFromDOM('.my-gallery');
+</script>
+<?php endif;?>
 </body>
 </html>
